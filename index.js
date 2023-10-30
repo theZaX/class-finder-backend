bca180db1c34c19f86dfcb2fd4d35d7637072522
jsbarre1 <john.bearett@gmail.com>
2023-10-30 12:05:37 -0500

    Now returns a final array which contains the 10 closest classes. When they search for english connect it will return both english connect 1 and english connect 2

--- index.js
+++ index.js
@@ -19,39 +19,59 @@ app.get('/map', async (req, res) => {
    const location = req.query.location
    let requestedOffering = req.query.offering
 
-  // for offering filter by an array of class offerings
-  let offeringFilterArray = [requestedOffering]
 
-  if(requestOffering=== "EnglishConnect"){
-    offeringFilterArray= ["EnglishConnect 1", "EnglishConnect 2"]
-  }
 
 
   const result = await geocode(location);
-  const formattedAddress = result.formatted_address;
-
+  const targetLat = result.geometry.location.lat;
+  const targetLng = result.geometry.location.lng;
+  console.log(targetLat + " " + targetLng);
+  // res.send(result);
   const client = await getConnectedClient();
   const classesData= await client.sql`select address_formatted, id, days_class_held, start_time, lat, lng, city, class_offering from master_calendar`
 
   //creates an array with classes containing the distance from the provided location
 
-  const classesWithDistance = classesData.data.map((classRecord) => {
-    const distanceBetween = calculateDistance(
-      classRecord.lat, 
-      classRecord.lng, 
-      //geocode result1,
-      //geocode result2
-    );
-  
-    return {
-      ...classRecord,
-      distanceBetween
-    };
+
+const classesWithDistance = classesData.rows.map((classRecord) => {
+      const distanceBetween = calculateDistance(
+        classRecord.lat, 
+        classRecord.lng, 
+        targetLat,
+        targetLng
+      );
+    
+      return {
+        ...classRecord,
+        distanceBetween
+      };
   });
 
-//filter classses with distance based on the distance from the provided address then slice it to the top 10 closest classes
+classesWithDistance.sort((a,b) => a.distanceBetween - b.distanceBetween);
+//if the requested class is english connect then filter for both english connect one and two
+let filteredArray;
+if(requestedOffering === "EnglishConnect"){
+  filteredArray = classesWithDistance.filter((classData) => {
+    if(classData.class_offering === "EnglishConnect 1" || classData.class_offering === "EnglishConnect 2" ){
+      return true;
+    }
+    else return false;
+  })
+}
+else{
+  //if it is anything else filter based on the requested offering query
+  filteredArray = classesWithDistance.filter((classData) => {
+    if(classData.class_offering === requestedOffering ){
+      return true;
+    }
+    else return false;
+  })
+}
 
-  res.json(data)
+//slices the top 10 closest classes
+const finalArray = filteredArray.slice(0,10);
+//sends the final array
+res.send(finalArray);  
 })
 
 
